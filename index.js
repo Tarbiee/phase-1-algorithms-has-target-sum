bb39827283ddb7259eb2bd7707e97a347650c1e9
Tarbiee <dorcusstaby@gmail.com>
2023-10-06 11:30:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for(let i=0; i<array.length; i++){
+    const curNum = array[i];
+    for(let j=i+1; j<array.length; j++){
+      if((curNum +array[j])=== target){
+        return true;
+      }
+    }
+
+    }
+    return false;
+    
+  }
+
+
 
 /* 
   Write the Big O time complexity of your function here
